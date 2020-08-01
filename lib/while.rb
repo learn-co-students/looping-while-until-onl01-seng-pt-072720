cd5c5ff6a507a037d6a2f770cb16e012f21d3a77
Javier Aguilar <coyote176@msn.com>
2020-08-01 19:05:45 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,9 @@
 def using_while
-  levitation_force = 6
-	
-  #your code here
+  levitation_force = 7
+	  while levitation_force <= 10
+	    puts "Wingardium Leviosa"
+	      levitation_force += 1
+  end
 end
-
+#This will print it 5 times, but we need 4!
 
